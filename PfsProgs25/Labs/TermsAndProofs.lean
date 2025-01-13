ec44325a262a7e8ae5da96a7904f29c09905e130
Mrigank Pawagi <mrigankpawagi@gmail.com>
2025-01-13 23:56:12 +0530

    free

--- PfsProgs25/Labs/TermsAndProofs.lean
+++ PfsProgs25/Labs/TermsAndProofs.lean
@@ -1,1 +1,83 @@
-jCgh7FwH1R/bDfVwb/AWj05qnbuXhLTNHbusjoXCh7zb57jUKJ8jjNGyCOCxHhOpLfMVyliURhCjEP4h+0htIVSWfjil1+K72IKftzMgwRuLTJ+6dqcaaqUF5ReSIApIuW0+PENwzG0zL7+xQsZHNcqEyDlHIamKnPpKjeoCUMRMov1bQl8Dgb10P10CZlJz7cmMjN3NLNqXooeMyS7vI8yHmzrSQbzte0GnhSg7vRV/8qP8RwXA2dvTQM9uVh7f+xoye6yHFGk5joB9oKB2IvzwhYrE9+PXHMusQeXcdG+LO1YRx3m2yZM4ECvJKk3uhSodzJbHNiVv0y7FDhohrHtHMXcD4FQQ/UEhDlOczfol6HHjHfmSZ572RvH3UgDbOmXAczDcG4bDvR3FbBkoK0BkqG2gotlY5L+U052NzabzWVBjx8xXWxlShfI+Umq4wz7mvDd9525lZiMPTb9nD4dS5tecEC6cFq8S41WJLWpTzMQHetcWTUip/nwtMg3+BStPaXfM8vU87rJgDjprwK7sTDqIAp/M8A1bYj69qNMrYCed0PDagCJcu+GZKksbSbG88wjqS3e/M9+/XwzhZUh4IoBI5GWIqxSH5AYdJz/L6QP3JDo7LqZd3Pi3j/NhGxdFXBOg5+ePkff/BwkvDNoHpaAK8fHLj6+ZQ2kri0d5XGtPrGzmEVfi02TeeVZA+G6jle1lt1p8+BzZmyw13QQx6vw3Lqx8Wuv9KXg8WyB6yRyI5JHcI3Y3mUdSLjONO8r7pJYZCG0RGFAOMrwFiKic8+jWYwE8py+gsA+K48eLa6P1b8Qit9nJa4ztOpINNjvtTCxBkvTdKZ4Xs3SJ9do9m8l6lJwO/QxGpDS2y8xIWlDB9Si+NopRraAV6+vSCjyskR0mY0nsqwDi+SfvP0hfXnyusDL20fANREghB+0oKpCDpV5Vk4ZQpalq6AeTITFMhpbHF9L8jKc6nVCdIY4j/ijbjqHYtPw811M9Z08xMrYaXa65JlyzPHzryssDCPWJUk0Q8j383Rf250CgPuf756kmKt7is4VI/PLLELSUGVWpzyk8ypfuSkcFUJlud3bdg0ewFZPV069iMKPwkLPG93Z00knIMlClh7ZvSyRabeT03yRrKTkIV/eUMlKSeNZ/sLiXu86K/Cu0DJvNrw1Tutc4TjxAX+WEpZeU2blLAJ0mCqjsWOagBC3+Oc+lhunFPAK5KwlVlkinhUrLJ32ZdxU+XJQ0+6AVhF/y4EBrm7SaGUbAKUoSOdgNPP6BECVsBq0kNiZrvUvDDOvrPZ9jmN1l5BkzH42rv5MHGaN/YPORi6DlnenMMhRFo6rk/6Ln94/KjSD/OlxW2GIDlHAgtahFwwbmBpUUvhH4AFI8oEEMnvYh01hIx+u2zhwy/MWE2zaeCgOQ8pilOAs39nb3LazBoH8HaUv33a4HPVkkfJttsI1LtMTgxyYnaMzGkZrx9CV+2+WyMaTRxLlLBfcsQWDoY1j7VkOGTQ2Icr5xCCcH4hyzB6CLCSq8rIbA4SvJlmYWOob8/RjkrPnfSzLtQieMRjr1npfdm5YKP65lqGD12FJydB1i7RuHRo/otrCqjrYVwnLKfKJidlIwy54UuGj5fC83gsariCIZACjcKyGtBd6dSd9scgooWT+Ab26ByeAP8yf60+cqIc/B7I2hrUVGhzEh+RjH/48gUxhoPCoUn1IAZCwMCpY37/rVzxmyWLvp/Ex0vvaoqUfvv4yqB/1TVftSYavdeKRNGbC5oa/sjHxyLx44xuD0AwQnzx5ah9kkzl3B5N3COzNqnRuxXjSxSRwomoYj0iPRlSouoOS/FZMJUwm6ofZ8CwBu36rz/GYldpCOReu/5D78nJc2T+0liEQoWyuugQMbUCJIrY0KrzR711rpK4DoP3JIcjohuJ/5GycwYAxBifou+8DzHCkSDLbgh0fVExJvozYHj7NizFN/eLsuhs3wuzqZxWDilqxpfEB9JtzmRZkC1m92ZDnsgcT0ka0YaSR9SmM9AHHIAaTxQgc6HK+F5FCeWiRo5FeIaxMbCNDreHKeRXkDeH/bCArSRfZkg79J3xEeNQ55r3l6PLnFcqdhrfy8wMKA2gqru480VTdM7/od/RwvtbjJkilubc1O8FZH6hDsftL+PVVKIq4Lp15p7toQpbW1PimtJkAX9znB3oZug75KfoSq9nKy/Qzk+yPVGAzwM8mAo94AN0nrrDFqLQUj2C6FnHznnvkF5odPDpKFF/9ZM6rG3N9804MYjw7T3C1GJPCmNsgTrvWSKZT66u2W/Rx0wNHaussUNRSwjnoNIeNtCjOnowBL+9uYJbDYWoTxWNLZ2/myFEAEF2iOqfj09nzARdVsPJLDiLccOSZYO2z96QxDG5F1XD5C5lheyHMAe0y/zZxI/q9D2B6p7MI0lLcF4y90PLs8qm5z94BMB8qU2YZyBeBnAx20/+Ga7f8wjgkRFbOOzvqAMDICKLik0Z2GhnzP28YHYsLQMCvLLJv9+V4yovjH2Qr0IUdtVNmzGi7B8rov0vu4rBYPtoV+67+WmzHvGizc0p2+yvzNBQKEfjd7bgZSQENpu6n2YFAeSPkzwIdIcJhKu6GeMfF7q4CrxJtWt5t9VQqUE9qG4yD8rXCqeKjpf/LZWG3RZhm7OFIpjSZiEDXMNFgrLMsnNPREnHeVXELtrteBTFJJUpt7LosRiTgMisYn3caASpEOH+wY5FeXRYJ51y+4B5RM71Qth4u9uTXDey8GCytaej2cPK2eqJbbpJorWvw+j2wbh185xYrTfP/YJoq6V1YRkHN4VCDBguuQOE3oTjuNPFz4ZyuBUakURtCFil4EnuT/Gxg9aTpdAbz0nWE6aVZZp+sBuY2zP5PSefspYWhzRj80D/3bZ0YSz1bEpxBKpW+G6V+OcvDcOqt6Dk2RNjGQh3JWqeks7Oopj+kQaDr7p3/LcdK91P5XbOge/ksF5Q5GNoSzBnlaSp1PE+Dx/k+co9KUEYtVyJMQ7cySDpG5WopktACosK+LoRCNe3v2PoY=
+/-!
+# Lab 01: Terms and Proofs
+
+In this lab, you will define a few terms and prove some simple theorems. The proofs should all be **term** proofs, i.e., without using tactics.
+
+To solve this lab, you should replace `sorry` with appropriate terms and proofs. The `sorry` command is a placeholder for code that has not been written yet. It is used as a temporary stub for an incomplete proof.
+
+Some tests are also included as code that is commented out. If your code is correct these should pass after uncommenting.
+
+## Some Terms
+
+Define an `Int` term eight that is equal to 8.
+-/
+def eight : sorry := sorry
+
+/-!
+**Test:** Uncomment the following lines to test your definition.
+-/
+-- /--
+-- error: type mismatch
+--   eight
+-- has type
+--   Int : Type
+-- but is expected to have type
+--   Nat : Type
+-- -/
+-- #guard_msgs in
+-- example : Nat := eight
+
+/-#
+## Simple functions
+
+Define two functions `f` and `g` that map a natural number `n` to `2 * n + 1`, using the argument and the λ syntax respectively.
+-/
+
+def f (n: Nat) : Nat := sorry
+
+def g : Nat → Nat := fun n ↦ sorry
+
+/-!
+**Test:** Uncomment the following lines to test your definition.
+-/
+-- example : f 3 = 7 := rfl
+-- example : g 3 = 7 := rfl
+
+/-!
+## Curried functions
+
+Rewrite the functions `h` and `composeSelf` which are in argument form to curried form as `h'` and `composeSelf'`.
+-/
+def h (m : Int) (n: Nat) : Int := m - n
+
+def h' : Int → Nat → Int := sorry
+
+def composeSelf (f : Nat → Nat) (n : Nat) : Nat := f (f n)
+
+def composeSelf' : (Nat → Nat) → (Nat → Nat) := sorry
+
+/-!
+**Test:** Uncomment the following lines to test your definition.
+-/
+-- example : h' 5 3 = 2 := rfl
+-- example : composeSelf' (fun n ↦ n + 1) 3 = 5 := rfl
+
+/-!
+## Term Proofs
+
+In the following, prove the result using only the theorems `Nat.le_refl` and `Nat.le_step` and without using tactics. In particular, do not use `Nat.succ_le_succ`.
+-/
+#check Nat.le_refl -- ∀ (n : ℕ), n ≤ n
+
+#check Nat.le_step -- ∀ (n m : ℕ), n ≤ m → n ≤ m + 1
+
+example : 3 ≤ 3 := sorry
+
+example : 3 ≤ 5 := sorry
+
+example (n: Nat) : n ≤ n + 2 := sorry
+
+-- change
+example : 3 ≤ 5 := sorry
+
+example (n: Nat) : n ≤ n + 2 := sorry
